5e34c1d17acfaa15cc534532bb950a1591f286b7
alexsanjoseph <alexsanjoseph@gmail.com>
2020-05-01 00:15:09 +0530

    Sort done!

--- bodybuilder/builder.py
+++ bodybuilder/builder.py
@@ -1,7 +1,7 @@
 """
 Main Builder Class and functions
 """
-
+from collections import OrderedDict
 
 class BodyBuilder:
 
@@ -15,24 +15,54 @@ class BodyBuilder:
         self.orFilters = []
         self.notFilters = []
         self.aggs = []
+        self.sorts = OrderedDict()
         self.body = {}
 
-    def _add_queries_simple(self):
-        simple_query = self.queries[0]
-        query_dict = {
-            simple_query[0]: {}
+    def _add_bool_struct(self):
+        self.body['query'] = {
+            'bool': {}
         }
-        if simple_query[1] is not None:
-            query_dict[simple_query[0]] = {
-                simple_query[2]: simple_query[1]
+
+    @staticmethod
+    def create_generic_query(*args):
+        query_dict = {
+            args[0]: {}
+        }
+        if len(args) > 1:
+            query_field = 'field' if len(args) == 2 else args[1]
+            query_val = args[-1]
+            query_dict[args[0]] = {
+                query_field: query_val
             }
-        self.body['query'] = query_dict
+        if len(args) > 3:
+            raise ValueError
+        return query_dict
+
+    @staticmethod
+    def create_sort_query(sort_dict):
+        sort_body = [{key: {'order': value}}for key, value in sort_dict.items()]
+        return sort_body
+
+    def _add_queries_simple(self):
+        self.body['query'] = self.create_generic_query(*self.queries[0])
 
     def _add_queries(self):
-        pass
+        if len(self.queries) == 0:
+            return
+        if len(self.queries) <= 1:
+            filter_dict = self.create_generic_query(*self.queries[0])
+            self.body['query']['bool']['must'] = filter_dict
+        else:
+            raise NotImplementedError
 
     def _add_filters(self):
-        pass
+        if len(self.filters) == 0:
+            return
+        if len(self.filters) <= 1:
+            filter_dict = self.create_generic_query(*self.filters[0])
+            self.body['query']['bool']['filter'] = filter_dict
+        else:
+            raise NotImplementedError
 
     def _add_or_filters(self):
         pass
@@ -40,12 +70,13 @@ class BodyBuilder:
     def _add_not_filters(self):
         pass
 
-    def query(self, query_type, query_val=None, query_field='field'):
-        self.queries.append((query_type, query_val, query_field))
-        return self
+    def _add_sort(self):
+        if len(self.sorts) == 0:
+            return
+        sort_dict_list = self.create_sort_query(self.sorts)
+        self.body['sort'] = sort_dict_list
 
-    def filter(self, type, field, value):
-        pass
+    ######################
 
     def is_simple_query(self):
         if len(self.queries) != 1:
@@ -54,15 +85,48 @@ class BodyBuilder:
             return False
         return True
 
+    def query_exists(self):
+        if (len(self.queries)
+            + len(self.filters)
+            + len(self.orFilters)
+            + len(self.notFilters)
+            ) > 0:
+            return True
+        return False
+
+    def query(self, *args):
+        self.queries.append(args)
+        return self
+
+    def filter(self, *args):
+        self.filters.append(args)
+        return self
+
     def build(self):
-        if self.is_simple_query():
-            self._add_queries_simple()
-        else:
-            self._add_queries()
-            self._add_filters()
-            self._add_or_filters()
-            self._add_not_filters()
+        if self.query_exists():
+            if self.is_simple_query():
+                self._add_queries_simple()
+            else:
+                self._add_bool_struct()
+                self._add_queries()
+                self._add_filters()
+                self._add_or_filters()
+                self._add_not_filters()
+        self._add_sort()
         return self.body
 
-    def get_query(self):
-        return self.build()['query']
+    def sort(self, *args):
+        if len(args) > 2:
+            raise ValueError
+        sort_type = 'asc' if len(args) == 1 else args[1]
+        self.sorts[args[0]] = sort_type
+        return self
+
+    def getQuery(self):
+        if self.is_simple_query():
+            return self.build()['query']
+        else:
+            return self.build()['query']['bool']['must']
+
+    def getFilter(self):
+        return self.build()['query']['bool']['filter']
--- tests/test_builder.py
+++ tests/test_builder.py
@@ -2,22 +2,22 @@
 This file holds all the main tests
 """
 
-from bodybuilder.builder import BodyBuilder as BB
+from bodybuilder.builder import BodyBuilder as bodyBuilder
 
 
 def test_query_no_field():
 
-    result = BB().query('match_all')
+    result = bodyBuilder().query('match_all')
 
     expected_query = {
         'match_all': {}
     }
 
-    assert result.get_query() == expected_query
+    assert result.getQuery() == expected_query
 
 
 def test_exist_user():
-    result = BB().query('exists', 'user')
+    result = bodyBuilder().query('exists', 'user')
 
     expected_query = {
         'exists': {
@@ -25,4 +25,107 @@ def test_exist_user():
         }
     }
 
-    assert result.get_query() == expected_query
+    assert result.getQuery() == expected_query
+
+
+def test_filter_without_query():
+    result = bodyBuilder() \
+        .filter('term', 'user', 'kimchy') \
+        .build()
+
+    expected_query = {
+        'query': {
+            'bool': {
+                'filter': {
+                    'term': {
+                        'user': 'kimchy'
+                    }
+                }
+            }
+        }
+    }
+
+    assert result == expected_query
+
+
+def test_query_and_filter():
+    result = bodyBuilder() \
+        .query('exists', 'user') \
+        .filter('term', 'user', 'kimchy')
+
+    expected_query = {
+        'exists': {
+            'field': 'user'
+        }
+    }
+    expected_filter = {
+        "term": {
+            "user": 'kimchy'
+        }
+    }
+
+    assert result.getQuery() == expected_query
+    assert result.getFilter() == expected_filter
+
+
+def test_filtered_query():
+    result = bodyBuilder() \
+        .query('match', 'message', 'this is a test') \
+        .filter('term', 'user', 'kimchy') \
+        .build()
+
+    expected_query = {
+        'query': {
+            'bool': {
+                'must': {
+                    'match': {
+                        'message': 'this is a test'
+                    }
+                },
+                'filter': {
+                    'term': {
+                        'user': 'kimchy'
+                    }
+                }
+            }
+        }
+    }
+
+    assert result == expected_query
+
+
+def test_basic_sort():
+    result = bodyBuilder().sort('timestamp').build()
+    expected_query = {
+        "sort": [
+            {
+                'timestamp': {
+                    "order": 'asc'
+                }
+            }
+        ]
+    }
+    assert result == expected_query
+
+
+def test_advanced_sort():
+    result = bodyBuilder() \
+        .sort('A', 'desc') \
+        .sort('B', 'desc') \
+        .sort('A') \
+        .build()
+    expected_query = {
+        "sort": [
+            {
+                "A": {
+                    "order": "asc"
+                }
+            },
+            {
+                "B": {
+                    "order": "desc"
+                }
+            }
+        ]
+    }
+    assert result == expected_query
