6f24bc5b0a306882c7cba16feef62e9876bfe0d1
alexsanjoseph <alexsanjoseph@gmail.com>
2020-05-02 09:51:34 +0530

    Update README.md

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,131 +1,127 @@
 """
 This file holds all the main tests
 """
+from unittest import TestCase
 
 from bodybuilder.builder import BodyBuilder as bodyBuilder
 
 
-def test_query_no_field():
+class TestBodyBuilder(TestCase):
 
-    result = bodyBuilder().query('match_all')
+    def test__query_no_field(self):
+        result = bodyBuilder().query('match_all')
 
-    expected_query = {
-        'match_all': {}
-    }
-
-    assert result.getQuery() == expected_query
-
-
-def test_exist_user():
-    result = bodyBuilder().query('exists', 'user')
-
-    expected_query = {
-        'exists': {
-            'field': 'user'
+        expected_query = {
+            'match_all': {}
         }
-    }
 
-    assert result.getQuery() == expected_query
+        assert result.getQuery() == expected_query
 
+    def test__exist_user(self):
+        result = bodyBuilder().query('exists', 'user')
 
-def test_filter_without_query():
-    result = bodyBuilder() \
-        .filter('term', 'user', 'kimchy') \
-        .build()
+        expected_query = {
+            'exists': {
+                'field': 'user'
+            }
+        }
 
-    expected_query = {
-        'query': {
-            'bool': {
-                'filter': {
-                    'term': {
-                        'user': 'kimchy'
+        assert result.getQuery() == expected_query
+
+    def test__filter_without_query(self):
+        result = bodyBuilder() \
+            .filter('term', 'user', 'kimchy') \
+            .build()
+
+        expected_query = {
+            'query': {
+                'bool': {
+                    'filter': {
+                        'term': {
+                            'user': 'kimchy'
+                        }
                     }
                 }
             }
         }
-    }
 
-    assert result == expected_query
+        assert result == expected_query
 
+    def test__query_and_filter(self):
+        result = bodyBuilder() \
+            .query('exists', 'user') \
+            .filter('term', 'user', 'kimchy')
 
-def test_query_and_filter():
-    result = bodyBuilder() \
-        .query('exists', 'user') \
-        .filter('term', 'user', 'kimchy')
-
-    expected_query = {
-        'exists': {
-            'field': 'user'
+        expected_query = {
+            'exists': {
+                'field': 'user'
+            }
         }
-    }
-    expected_filter = {
-        "term": {
-            "user": 'kimchy'
+        expected_filter = {
+            "term": {
+                "user": 'kimchy'
+            }
         }
-    }
 
-    assert result.getQuery() == expected_query
-    assert result.getFilter() == expected_filter
+        assert result.getQuery() == expected_query
+        assert result.getFilter() == expected_filter
 
+    def test__filtered_query(self):
+        result = bodyBuilder() \
+            .query('match', 'message', 'this is a test') \
+            .filter('term', 'user', 'kimchy') \
+            .build()
 
-def test_filtered_query():
-    result = bodyBuilder() \
-        .query('match', 'message', 'this is a test') \
-        .filter('term', 'user', 'kimchy') \
-        .build()
+        expected_query = {
+            'query': {
+                'bool': {
+                    'must': {
+                        'match': {
+                            'message': 'this is a test'
+                        }
+                    },
+                    'filter': {
+                        'term': {
+                            'user': 'kimchy'
+                        }
+                    }
+                }
+            }
+        }
 
-    expected_query = {
-        'query': {
-            'bool': {
-                'must': {
-                    'match': {
-                        'message': 'this is a test'
+        assert result == expected_query
+
+    def test__basic_sort(self):
+        result = bodyBuilder().sort('timestamp').build()
+        expected_query = {
+            "sort": [
+                {
+                    'timestamp': {
+                        "order": 'asc'
+                    }
+                }
+            ]
+        }
+        assert result == expected_query
+
+    def test__advanced_sort(self):
+        result = bodyBuilder() \
+            .sort('A', 'desc') \
+            .sort('B', 'desc') \
+            .sort('A') \
+            .build()
+        expected_query = {
+            "sort": [
+                {
+                    "A": {
+                        "order": "asc"
                     }
                 },
-                'filter': {
-                    'term': {
-                        'user': 'kimchy'
+                {
+                    "B": {
+                        "order": "desc"
                     }
                 }
-            }
+            ]
         }
-    }
-
-    assert result == expected_query
-
-
-def test_basic_sort():
-    result = bodyBuilder().sort('timestamp').build()
-    expected_query = {
-        "sort": [
-            {
-                'timestamp': {
-                    "order": 'asc'
-                }
-            }
-        ]
-    }
-    assert result == expected_query
-
-
-def test_advanced_sort():
-    result = bodyBuilder() \
-        .sort('A', 'desc') \
-        .sort('B', 'desc') \
-        .sort('A') \
-        .build()
-    expected_query = {
-        "sort": [
-            {
-                "A": {
-                    "order": "asc"
-                }
-            },
-            {
-                "B": {
-                    "order": "desc"
-                }
-            }
-        ]
-    }
-    assert result == expected_query
+        assert result == expected_query
